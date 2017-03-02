1cb1759ae013355149f0c8a12fe5422c091b1443
Pratik948 <jamariyapratik@gmail.com>
2017-03-03 02:43:01 +0530

    Added UIViewController+TopViewController

---
+++ UIViewController+TopViewController/UIViewController+TopViewController.h
@@ -0,0 +1,14 @@
+//
+//  UIViewController+TopViewController.h
+//  The10Men
+//
+//  Created by Pratik on 03/03/17.
+//  Copyright © 2017 The10Men. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (TopViewController)
+- (UIViewController *)topViewController;
+@end
+
---
+++ UIViewController+TopViewController/UIViewController+TopViewController.m
@@ -0,0 +1,31 @@
+//
+//  UIViewController+TopViewController.m
+//  The10Men
+//
+//  Created by Pratik on 03/03/17.
+//  Copyright © 2017 The10Men. All rights reserved.
+//
+
+#import "UIViewController+TopViewController.h"
+
+@implementation UIViewController (TopViewController)
+
+- (UIViewController *)topViewController {
+    return [self topViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
+}
+
+- (UIViewController *)topViewController:(UIViewController *)rootViewController
+{
+    if (rootViewController.presentedViewController == nil) {
+        return rootViewController;
+    }
+    if ([rootViewController.presentedViewController isMemberOfClass:[UINavigationController class]]) {
+        UINavigationController *navigationController = (UINavigationController *)rootViewController.presentedViewController;
+        UIViewController *lastViewController = [[navigationController viewControllers] lastObject];
+        return [self topViewController:lastViewController];
+    }
+    UIViewController *presentedViewController = (UIViewController *)rootViewController.presentedViewController;
+    return [self topViewController:presentedViewController];
+}
+
+@end
